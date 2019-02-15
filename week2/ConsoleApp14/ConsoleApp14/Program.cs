4217e6b6dc572b7c5746ed1832464d7daae964b8
mrabiii <rabiga001@bk.ru>
2019-02-16 04:05:33 +0600

    tf

---
+++ week2/ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        public static bool isPol(string s) //create function to check for polindrome
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static void Main(String[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Гульмира\Desktop\text.txt"); //reads file
+            string s = sr.ReadToEnd();
+            sr.Close(); //closes sr
+            if (isPol(s)) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+            Console.ReadKey();
+        }
+
+    }
+}
