a545515b9ee8dc82a674d7e94842c6e1336b0e0d
mrabiii <rabiga001@bk.ru>
2019-02-16 10:48:57 +0600

    done

--- week2/ConsoleApp15/ConsoleApp15/Program.cs
+++ week2/ConsoleApp15/ConsoleApp15/Program.cs
@@ -7,40 +7,37 @@ using System.IO;
 using System.Xml.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
-namespace ConsoleApp8
+
+namespace ConsoleApp15
 {
     class Program
     {
-        public static void WhiteSpace(int level)
+        public static bool isPrime(int n)
         {
-            for (int i = 0; i < level * 4; i++)  //create array which make space before file
-
+            if (n == 1) return false;
+            for (int i = 2; i < n; i++)
             {
-                Console.Write(' ');
+                if (n % i == 0) return false;
             }
-        }
-
-        public static void print(string path, int level)
-        {
-            DirectoryInfo directoryInfo = new DirectoryInfo(path);
-            FileInfo[] fileinfos = directoryInfo.GetFiles();
-            DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
-            foreach (FileInfo f in fileinfos)
-            {
-                WhiteSpace(level);
-                Console.WriteLine(f.Name);
-            }
-            foreach (DirectoryInfo d in directoryInfos)
-            {
-                WhiteSpace(level);
-                Console.WriteLine(d.Name);
-                print(d.FullName, level + 1);
-            }
+            return true;
         }
         public static void Main(String[] args)
         {
-            print(@"C:\Users\Гульмира\Desktop\pp2", 0);
-            Console.ReadKey();
+            StreamReader sr = new StreamReader(@"C:\Users\ГУльмира\Desktop\text.txt");
+            string[] arr = sr.ReadToEnd().Split(' ');
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\Гульмира\Desktop\output.txt");
+            string res = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (isPrime(int.Parse(arr[i])))
+                {
+                    res += arr[i] + ' ';
+                }
+            }
+            sw.Write(res);
+            sw.Close();
         }
     }
 }
